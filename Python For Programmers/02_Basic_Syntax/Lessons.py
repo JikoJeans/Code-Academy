7cb86b9ca49d4b651df26ae3c1993c1d524e793e
Miguel Munguia <miguelmung5@gmail.com>
2024-04-07 19:26:37 -0700

    Create Lessons.py
    This pull request contains the creation of the notes file and the notes for the first lesson.

---
+++ Python For Programmers/02_Basic_Syntax/Lessons.py
@@ -0,0 +1,29 @@
+#Lesson One:       Program Structure
+    #Hello World
+        # The notorous hello world program can be written in Python as follows:
+print('Hello World!')
+    #The main() Function
+def main():
+    print('Hello World!')
+        # The main function defines the starting point of the program and including
+        #  main() allows us to import and run the program in other scripts.
+if __name__ == '__main__':
+    main()
+        # The if statement checks whether the program is being run independently as
+        #  the primary module or as a library in another script.
+    #Examples
+def main():
+    print('New World!')
+    newFunction()
+
+def newFunction():
+    print('Second Output')
+
+if __name__ == '__main__':
+    main()
+    
+#Lesson Two:       Comments
+#Lesson Three:     Input and Output
+#Lesson Four:      Variables
+#Lesson Five:      Data Types
+#Lesson Six:       Operators
