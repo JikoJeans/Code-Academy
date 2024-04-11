79a9d2ec1c5cb1ef93add4874635a53a83c3da3e
Miguel Munguia <miguelmung5@gmail.com>
2024-04-10 21:33:14 -0700

    Update Lessons.py
    This update contains the notes for lesson 3 and is about tuples in python.

--- Python For Programmers/06_Data_Structures/Lessons.py
+++ Python For Programmers/06_Data_Structures/Lessons.py
@@ -1,7 +1,7 @@
 #Lesson One:       String Methods
     # Strings in Python
         # In Python, strings are defined with single or double qoutes('' or "")
-        # and can contain a combination of letters, numbers, symbols, and spaces.
+        #  and can contain a combination of letters, numbers, symbols, and spaces.
     # String Indexing
         # As mentioned above, a string is a combination of letters, numbers,
         #  symbols, and spaces and this list of characters can be accessed by
@@ -14,7 +14,7 @@ print(test_string[0])  #  prints T
         #  first 2 letters of string we could declare that as follows:
 start = test_string[0:2]  #  Would contain 'Te'
         # Negative indices can also be applied to grab from the back end
-        # of the string like in the example below:
+        #  of the string like in the example below:
 end = test_string[-3:-1]  #  Would contain 'in'
 
     # Common Built-In Functions
@@ -57,16 +57,16 @@ random_list = ['The', 66, 'def', 2.1, ['e', 'h', 'd']]
 
     # List Indexing and Slicing
         # Items in a list can be accessed similar to strings utilizing the index.
-        # Using brackets again we start the selection at the 0 index and moving up
-        # from there. Slicing works as anticipated (similar to strings) and can be
-        # seen in the demonstration below:
+        #  Using brackets again we start the selection at the 0 index and moving up
+        #  from there. Slicing works as anticipated (similar to strings) and can be
+        #  seen in the demonstration below:
 print(numbered_list[0])  #  39
 print(random_list[1:3])  #  [66, 'def']
 
     # Common Built-in Methods
         # len()
             # The length of a list can be measured using the built-in function len()
-            # by passing in the list. Below shows the call on both lists:
+            #  by passing in the list. Below shows the call on both lists:
 print(len(numbered_list))  #  4
 print(len(random_list))  #  5
 
@@ -83,11 +83,70 @@ random_list.remove('def')  #  removes 'def' from the random list
 
         # .pop()
             # The .pop() takes an index and removes the element given at that
-            # index. If no index is provided then the final element is poped.
+            #  index. If no index is provided then the final element is poped.
 numbered_list.pop()  #  removes 44 from the numbered list
 random_list.pop(0)  #  removes 'The' from the random list
 
 #Lesson Three:     Tuples
+    # Tuples
+        # Tuples are one of the built-in data structures in Python. Just like lists,
+        #  tuples can hold a sequence of items but they are more memory
+        #  efficient and have a slightly higher time efficiency than lists. This is
+        #  because tuples are immutable and as a result are useful when working with
+        #  data that does not need to be changed. In Python, tuples are defined with
+        #  parentheses () with comma-seperated values like in the example before:
+example_tuple = ('night', 32, 'day', 59)
+example2_tuple = ('evening',)  #  tuple with one element
+
+    # Tuples Indexing And Slicing
+        # Items in a tuple can be accessed using their index similar to lists. Slicing
+        #  works with the same notation utilizing  a colon ':' between the two indices
+        #  of the selection. Both examples can be found below:
+print(example_tuple[1])  #  32
+print(example2_tuple[0])  #  evening
+print(example_tuple[0:2])  #  ('night', 32)
+
+    # Common Built-in Functions
+        # len()
+            # The length of a tuple can be measured using the built-in function len().
+            #  It takes the tuple as an argument to count the items in the tuple
+            #  similar to list and can be seen below:
+print(len(example_tuple))  #  4
+print(len(example2_tuple))  #  1
+        # max()
+            # If all of the tuples values have the same data type then max() will
+            #  either return the largest numerical value or the string closest to
+            #  the letter "Z" like below:
+example3_tuple = (123, 999, 872)
+example4_tuple = ('first', 'second', 'third')
+print(max(example_tuple))  #  Throws an error
+print(max(example3_tuple))  #  999
+print(max(example4_tuple))  #  third
+
+        # min()
+            # the built-in min() function is limited similarly to max() but
+            #  functions in the reverse manor. The smallest numerical value or
+            #  string closes to the letter "A" like below:
+print(min(example_tuple))  #  Throws an error
+print(min(example3_tuple))  #  123
+print(min(example4_tuple))  #  first
+
+        # .index()
+            # The built-in method .index() takes in a value as the argument
+            #  and returns the index of that value if found. Two examples can
+            #  be found below:
+print(example3_tuple.index(999))  #  2
+print(example4_tuple.index('third'))  #  3
+
+        # .count()
+            # The build-in method .count() takes in a value as an argument
+            #  but returns the number of occurances found in the tuple. A few
+            #  examples can be found below:
+example5_tuple = ('abc', 'abc', 2, 3, 4)
+print(example3_tuple.index(9))  #  0
+print(example4_tuple.index('third'))  #  1
+print(example5_tuple.index('abc'))  #  2
+
 #Lesson Four:      Dictionaries
 #Lesson Five:      Sets
 #Lesson Six:       Queues and Stacks
